38303ec24ac9a6bcadf60e4442301ccb0f6a73e5
Danil <Danilshkr@gmail.com>
2023-04-12 21:16:31 +0700

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
+    return jsonify(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+    pass
+
+
+if __name__ == '__main__':
+    app.run()
